7ab07875aeedfffd8db33839c308921be40a81b2
naczyz <natan.czyz@gmail.com>
2023-09-05 08:50:53 +0200

    dodanie warsztatu

---
+++ js/main.js
@@ -0,0 +1,28 @@
+const btnGetData = document.getElementById("get-data");
+
+const getData = () => {
+  fetch("https://akademia108.pl/api/ajax/get-post.php")
+    .then((res) => res.json())
+    .then((data) => {
+      let pId = document.createElement("p");
+      let pUserId = document.createElement("p");
+      let pTitle = document.createElement("p");
+      let pBody = document.createElement("p");
+      let hr = document.createElement("hr");
+      pId.innerText = `Post ID: ${data.id}`;
+      pUserId.innerText = `User ID: ${data.userId}`;
+      pTitle.innerText = `Title ID: ${data.title}`;
+      pBody.innerText = `Body ID: ${data.body}`;
+
+      document.body.appendChild(pId);
+      document.body.appendChild(pUserId);
+      document.body.appendChild(pTitle);
+      document.body.appendChild(pBody);
+      document.body.appendChild(hr);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+};
+
+btnGetData.addEventListener("click", getData);
